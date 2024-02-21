a1301b49e2378999bdd197203b19078301a7e5d2
SNKariuki <karisstevo@gmail.com>
2024-02-21 12:19:46 +0300

    First version

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='mypackage',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
-    install_requires=['pandas', 'matplotlib', 'seaborn', 'scipy'],
     url='https://github.com/<username>/<package-name>',
     author='<Your Name>',
     author_email='<Your Email>'
